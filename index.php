405b770eb9fc276fc1be17686c123982aeaf5dfa
Fredii <79299980+Frediispkttt@users.noreply.github.com>
2021-05-19 21:37:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Fredi Supangkat
+203040076
+https://github.com/Frediispkttt/pw2021_203040076
+Pertemuan 2 - 26 Februari 2021
+Sintaks dasar PHP
+*/
+?>
+<?php
+// Standar Output
+// echo, print
+?>
